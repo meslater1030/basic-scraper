3f7858d5a129a4fa34846edeb071480831fa246d
Megan Slater <megan.e.slater@gmail.com>
2015-07-06 17:41:48 -0700

    add more to name equals main

--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+# from __future__ import unicode_literals
 import requests
 import json
 from bs4 import BeautifulSoup
@@ -50,8 +50,23 @@ def parse_source(html, encoding='utf-8'):
 
 
 def extract_data_listings(html):
-    id_finder = re.complie(r'PR[\d]+~')
-    return html.findall('div', id=id_finder)
+    id_finder = re.compile(r'PR[\d]+~')
+    return html.find_all('div', id=id_finder)
+
+
+def has_two_tds(element):
+    is_tr = element.name == 'tr'
+    td_children = element.find_all('td', recursive=False)
+    has_two = len(td_children) == 2
+    return is_tr and has_two
+
+
+def clean_data(td):
+    data = td.string
+    try:
+        return data.strip(" \n:-")
+    except AttributeError:
+        return u""
 
 
 if __name__ == '__main__':
@@ -64,8 +79,17 @@ if __name__ == '__main__':
         html, encoding = load_inspection_page()
     else:
         html, encoding = get_inspection_page(**kwargs)
-    import pdb; pdb.set_trace()
     doc = parse_source(html, encoding)
     listings = extract_data_listings(doc)
-    print len(listings)
-    print listings[0].prettify()
+    for listing in listings[:5]:
+        metadata_rows = listing.find('tbody').find_all(
+            has_two_tds, recursive=False
+            )
+        for row in metadata_rows:
+            for td in row.find_all('td', recursive=False):
+                print repr(clean_data(td))
+            print
+        print
+    # print len(metadata_rows)
+    # print len(listings)
+    # print listings[0].prettify()
